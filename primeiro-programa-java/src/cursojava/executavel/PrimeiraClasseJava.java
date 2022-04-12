d983f2230823b0d9cc466381350c6ee4c324435b
JoelCorreia5 <joel.junior86@gmail.com>
2022-04-12 17:44:28 -0300

    Comitado...

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,7 +1,7 @@
 package cursojava.executavel;
 
 public class PrimeiraClasseJava {
-	/*Main é um método auto executável em Java*/
+	/*Main é um método auto executável em Javaaaaaaaa*/
 	public static void main(String[] args) {
 		System.err.println("Olá Mundo!");
 	}
