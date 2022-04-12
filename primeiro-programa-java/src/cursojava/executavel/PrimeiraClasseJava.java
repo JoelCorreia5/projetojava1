61cf9a3e93179b6c502470b7f99592e1a2229689
JoelCorreia5 <joel.junior86@gmail.com>
2022-04-12 13:10:05 -0300

    Criação do Projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,9 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	/*Main é um método auto executável em Java*/
+	public static void main(String[] args) {
+		System.err.println("Olá Mundo!");
+	}
+	
+}
